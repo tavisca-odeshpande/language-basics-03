8f81c81ce3ff154359d8142ccd221065b41e4546
tavisca-odeshpande <52021545+tavisca-odeshpande@users.noreply.github.com>
2019-06-24 12:19:19 +0530

    Solution to assignment2-problem3

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
@@ -40,8 +41,105 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            int len = protein.Length;
+            int[] calorie = new int[len];
+            int[] meals = new int[dietPlans.Length];
+            for( int i = 0; i < len; i++)
+            {
+                calorie[i] = CalCalorie(protein[i], carbs[i], fat[i]);
+            }
+            for ( int i = 0; i < dietPlans.Length; i++)
+            {
+                var list = Enumerable.Range(0,len).ToList();
+                foreach ( char x in dietPlans[i])
+                {
+                    switch (x)
+                    {
+                        case 'C':
+                            list = FindLargest(carbs, list);
+                            break;
+                        case 'c':
+                            list = FindSmallest(carbs, list);
+                            break;
+                        case 'P':
+                            list = FindLargest(protein, list);
+                            break;
+                        case 'p':
+                            list = FindSmallest(protein, list);
+                            break;
+                        case 'F':
+                            list = FindLargest(fat, list);
+                            break;
+                        case 'f':
+                            list = FindSmallest(fat, list);
+                            break;
+                        case 'T':
+                            list = FindLargest(calorie, list);
+                            break;
+                        case 't':
+                            list = FindSmallest(calorie, list);
+                            break;
+                        default:
+                            Console.WriteLine("Default case");
+                            break;
+                    }
+                }
+                meals[i] = list[0];
+            }
+            
+            return meals;
+        }
+
+        private static List<int> FindSmallest(int[] arr,List<int> list)
+        {
+            int min=arr[list[0]];
+            List<int> newlist = new List<int>();
+            foreach (int x in list)
+            {
+                if(min > arr[x])
+                {
+                    newlist.Clear();
+                    min = arr[x];
+                    newlist.Add(x);
+                }else if(min == arr[x])
+                {
+                    newlist.Add(x);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return newlist;
+        }
+
+        private static List<int> FindLargest(int[] arr, List<int> list)
+        {
+            int max = arr[list[0]];
+            List<int> newlist = new List<int>();
+            foreach (int x in list)
+            {
+                if (max < arr[x])
+                {
+                    newlist.Clear();
+                    max = arr[x];
+                    newlist.Add(x);
+                }
+                else if (max == arr[x])
+                {
+                    newlist.Add(x);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return newlist;
+        }
+
+        private static int CalCalorie(int carb,int pro,int fat)
+        {
+            return carb * 5 + pro * 5 + fat * 9;
         }
     }
 }
